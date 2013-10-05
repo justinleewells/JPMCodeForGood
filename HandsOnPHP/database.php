1c082b72ddd059f30f78d285c2733e8d79f1e6b4
Justin L. Wells <wells.1069@osu.edu>
2013-10-04 23:30:04 -0400

    database class

--- HandsOnPHP/database.php
+++ HandsOnPHP/database.php
@@ -1,62 +1,56 @@
 <?php
-class Database {
-	protected $con;
-  
-	public function connect() {
-		$this->con = mysql_connect('ec2-184-73-92-152.compute-1.amazonaws.com','root','bitnami');
-		if(!$this->con) {
-			die("ERROR: Cannot connect to database!");
+$con = mysqli_connect('ec2-184-73-92-152.compute-1.amazonaws.com', 'root', 'bitnami', 'test', 3306) or die("FAILED");
+if ($con) {
+	echo("Worked");
+}
+
+function dbSelectWhere($table, $columns, $where_column, $where_operator, $where_value) {
+	if ($con) {
+		$result = mysql_query("SELECT " . $columns . " FROM " . $table . " WHERE " . $where_column . " " . $where_operator . " " . $where_value, $con);
+		if (!$result) {
+			die ("Invalid query " . mysql_error());
 		}
+		return $result;
 	}
-	
-	public function selectWhere($table, $columns, $where_column, $where_operator, $where_value) {
-		if ($this->con) {
-			$result = $this->con.mysql_query("SELECT " . $columns . " FROM " . $table . " WHERE " . $where_column . " " . $where_operator . " " . $where_value);
-			if (!$result) {
-				die ("Invalid query " . mysql_error());
-			}
-			return $result;
+}
+
+function dbSelectLike($table, $columns, $where_column, $like_value) {
+	if ($con) {
+		$result = mysql_query("SELECT " . $columns . " FROM " . $table . " WHERE " . $where_column . " LIKE '" . $like_value . "'", $con);
+		if (!$result) {
+			die ("Invalid query " . mysql_error());
 		}
+		return $result;
 	}
-	
-	public function selectLike($table, $columns, $where_column, $like_value) {
-		if ($this->con) {
-			$result = $this->con.mysql_query("SELECT " . $columns . " FROM " . $table . " WHERE " . $where_column . " LIKE '" . $like_value . "'");
-			if (!$result) {
-				die ("Invalid query " . mysql_error());
-			}
-			return $result;
+}
+
+function dbUpdate($table, $columns, $where_column, $where_operator, $where_value, $values) {
+	if ($con) {
+		$result = mysql_query("UPDATE " . $columns . " FROM " . $table . " WHERE " . $where_column . " " . $where_operator . " " . $where_value . " VALUES " . $values, $con);
+		if (!$result) {
+			die ("Invalid query " . mysql_error());
 		}
+		return $result;
 	}
-	
-	public function update($table, $columns, $where_column, $where_operator, $where_value, $values) {
-		if ($this->con) {
-			$result = $this-con.mysql_query("UPDATE " . $columns . " FROM " . $table . " WHERE " . $where_column . " " . $where_operator . " " . $where_value . " VALUES " . $values);
-			if (!$result) {
-				die ("Invalid query " . mysql_error());
-			}
-			return $result;
+}
+
+function dbInsert($table, $columns, $data) {
+	if ($con) {
+		$result = mysql_query("INSERT INTO " . $table . " FIELDS " . $columns . " VALUES " . $values, $con);
+		if (!$result) {
+			die ("Invalid query " . mysql_error());
 		}
+		return $result;
 	}
-	
-	public function insert($table, $columns, $data) {
-		if ($this->con) {
-			$result = $this-con.mysql_query("INSERT INTO " . $table . " FIELDS " . $columns . " VALUES " . $values);
-			if (!$result) {
-				die ("Invalid query " . mysql_error());
-			}
-			return $result;
+}
+
+function dbDrop($table, $where_column, $where_operator, $where_value) {
+	if ($con) {
+		$result = mysql_query("DROP FROM " . $table . " WHERE " . $where_column . " " . $where_operator . " " . $where_value, $con);
+		if (!$result) {
+			die ("Invalid query " . mysql_error());
 		}
-	}
-	
-	public function drop($table, $where_column, $where_operator, $where_value) {
-		if ($this->con) {
-			$result = $this-con.mysql_query("DROP FROM " . $table . " WHERE " . $where_column . " " . $where_operator . " " . $where_value);
-			if (!$result) {
-				die ("Invalid query " . mysql_error());
-			}
-			return $result;
-		}
+		return $result;
 	}
 }
 ?>
--- HandsOnPHP/test.php
+++ HandsOnPHP/test.php
@@ -1,4 +1,8 @@
 <?php
-$db = new Database();
-$db.connect();
+require("./database.php");
+/* $db = mysqli_connect('ec2-184-73-92-152.compute-1.amazonaws.com', 'root', 'bitnami', 'test', 3306) or die("FAILED");
+
+if ($db) {
+	echo("WORKED");
+}*/
 ?>
