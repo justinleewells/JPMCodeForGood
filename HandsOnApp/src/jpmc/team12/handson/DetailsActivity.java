9a5c09253bb72b746efcf3cfd93b1a4985e74286
cding9 <cding9@gatech.edu>
2013-10-05 10:51:12 -0400

    features

--- HandsOnApp/src/jpmc/team12/handson/DetailsActivity.java
+++ HandsOnApp/src/jpmc/team12/handson/DetailsActivity.java
@@ -1,22 +1,55 @@
 package jpmc.team12.handson;
 
+import jpmc.team12.handson.db.Credentials;
 import jpmc.team12.handson.db.Database;
 import jpmc.team12.handson.db.Event;
+import jpmc.team12.handson.db.OnDatabaseResultHandler;
 import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.text.Html;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.TextView.OnEditorActionListener;
 
 public class DetailsActivity extends Activity {
+	
+	private Button signUp;
+	private Button expressInterest;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_details);
 
+		signUp = (Button) findViewById(R.id.signupButton);
+		expressInterest = (Button) findViewById(R.id.expressInterestButton);
+		
+		signUp.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				performSignUp();
+			}
+		});
+		expressInterest.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				performExpressInterest();
+			}
+		});
+
+
+		
 		ActionBar actionBar = getActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);
 
@@ -58,5 +91,34 @@ public class DetailsActivity extends Activity {
 
 		return super.onOptionsItemSelected(item);
 	}
+	private void performSignUp() {
+		/*Intent intent = new Intent(getApplicationContext(),
+				ResultsActivity.class);
 
+		Bundle bundle = new Bundle();
+		bundle.putString("search", searchText.getText().toString());
+		intent.putExtras(bundle);
+		startActivity(intent);*/
+		
+		String username = findViewById(R.id.menu_account).getTag().toString();
+		if( username.equals("Guest")){
+			Toast.makeText(this, "Please Login or Register First",
+					Toast.LENGTH_LONG).show();
+		}else{
+			//Perform signUp functionality here
+		}
+
+	}
+	private void performExpressInterest() {
+		/*Intent intent = new Intent(getApplicationContext(),
+				ResultsActivity.class);
+
+		Bundle bundle = new Bundle();
+		bundle.putString("search", searchText.getText().toString());
+		intent.putExtras(bundle);
+
+		startActivity(intent);*/
+		Toast.makeText(this, "Interest Expressed",
+				Toast.LENGTH_SHORT).show();
+	}
 }
